4e8cdf9fab8f9256c125e5f3592879427748090f
asmalaaribi <laaribi.asm@gmail.com>
2017-10-23 19:44:22 -0400

    code formatting

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,14 +2,14 @@ import fresh_tomatoes
 import media
 
 
-# Creating 9 instances of the class Movie of the media Module 
+# Creating 9 instances of the class Movie of the media Module
 django = media.Movie("Django Unchained",
                      "https://goo.gl/sDByYA",
                      "https://www.youtube.com/watch?v=eUdM9vrCbow")
 
 shawshank_redemption = media.Movie("The Shawshank Redemption",
-                                   "https://goo.gl/BPqfPD",
-                                   "https://www.youtube.com/watch?v=6hB3S9bIaco")
+                        "https://goo.gl/BPqfPD",
+                        "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
 mean_girls = media.Movie("Mean Girls",
                          "https://goo.gl/c6KPZK",
@@ -32,19 +32,20 @@ jump_street = media.Movie("21 Jump Street",
                           "https://www.youtube.com/watch?v=ISJR4rVO0TQ")
 
 spiderman_homecoming = media.Movie("Spider-Man: Homecoming",
-                                   "https://goo.gl/Rn2ZJS",
-                                   "https://www.youtube.com/watch?v=n9DwoQ7HWvI")
+                            "https://goo.gl/Rn2ZJS",
+                            "https://www.youtube.com/watch?v=n9DwoQ7HWvI")
 
 bridesmaids = media.Movie("Bridesmaids",
                           "https://goo.gl/QFZEL8",
                           "https://www.youtube.com/watch?v=1UW9Zks5L2A")
 
-# Grouping the movies in a list 
+# Grouping the movies in a list
 favorite_movies = [mean_girls, the_big_sick, pitch_perfect,
                    jump_street, spiderman_homecoming, bridesmaids,
                    forrest_gump, django, shawshank_redemption]
 
 
 # The open_movies_page function takes a list of movies as its argument
-# It then creates an HTML file which will display all of the movies in the browser
+# It then creates an HTML file which will display all of the movies
+# in the browser
 fresh_tomatoes.open_movies_page(favorite_movies)
--- media.py
+++ media.py
@@ -2,9 +2,9 @@ class Movie():
     """ Movie class
 
     Attributes:
-        title: A string representing the title of the movie
-        poster_image_url: An string representing the url of the movie's poster
-        trailer_youtube_url: A string representing the url of the movie's trailer
+        title: string represents the title of the movie
+        poster_image_url: string representing the url of the movie's poster
+        trailer_youtube_url: string representing the url of the movie's trailer
     """
 
     # Constructor for the Movie class
@@ -12,5 +12,3 @@ class Movie():
         self.title = movie_title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-
-    
